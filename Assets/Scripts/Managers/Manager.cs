4f8f858634bb16d5973e4810ef61ad6cb2d31b87
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-01-21 11:48:07 -0700

    added a setter for the var visual ,<spriterenderer>

---
+++ Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public abstract class Manager : MonoBehaviour
+{
+    Camera cam;
+    readonly float mapUnitSide = 1.40625f;
+    [Header("Camera")]
+    public float size = 4.0f;
+    public void Start()
+    {
+        cam = Camera.main;
+        cam.orthographicSize = size * mapUnitSide;
+    }
+}
---
+++ Assets/Scripts/Managers/Platformer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteAlways]
+public class PlatformerManager : Manager
+{
+    Transform cameraTX;
+    
+    new void Start()
+    {
+        base.Start();
+        cameraTX = Camera.main.GetComponent<Transform>();
+    }
+}
+
+
